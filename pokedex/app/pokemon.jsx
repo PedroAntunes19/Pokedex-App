2015b8b2ce0875896633331d53d5fe5349757b25
pedroantunes19 <pedroantunes480@gmail.com>
2025-08-13 14:36:40 -0300

    terceiro commit

---
+++ pokedex/app/pokemon.jsx
@@ -0,0 +1,11 @@
+import { StylesSheet, Text, View } from 'react-native'
+
+export default function Pokemon() {
+    return (
+        <View>
+            <Text>pokemon</Text>
+        </View>
+    )
+}
+
+const styles = StylesSheet.create({})
